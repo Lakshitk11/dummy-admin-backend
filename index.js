820d9a47cb55b7a128c4ada08926147d23543a6a
Lakshit <karwalakshit@gmail.com>
2026-07-17 13:08:26 +0530

    Updated backend index.js

--- index.js
+++ index.js
@@ -38,5 +38,5 @@ app.use("/", router);
 app.use("/posts", postRoutes);
 
 app.listen(5000, () => {
-  console.log("🚀 Server running on port 5000");
+  console.log(" Server running on port 5000");
 });
